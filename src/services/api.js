96007c9fbe637d77ae96873ee741bc9dc1d176c1
Syedasif689 <asifsayed635@gmail.com>
2026-06-26 19:39:39 +0530

    updated server

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'https://playplus-backend-1.onrender.com/api',
+    baseURL: 'https://play-rltgyiwr.b4a.run/',
     headers: {
         'Content-Type': 'application/json',
     },
